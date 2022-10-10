cf88bf0eb0cb946dc34aef81469a02c9fbdd4703
Yaw Afrifa <isaac.afrifa3@yahoo.com>
2022-10-10 19:22:07 +0300

    Add ReadMe

--- src/main/java/com/iam/shoutz/ShoutzApplication.java
+++ src/main/java/com/iam/shoutz/ShoutzApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ShoutzApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ShoutzApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(ShoutzApplication.class, args);
+	}
 
 }
