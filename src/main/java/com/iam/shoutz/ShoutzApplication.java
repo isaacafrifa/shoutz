185fe9476d2ddd8f551a137c4d543da12dd94909
Yaw Afrifa <isaac.afrifa3@yahoo.com>
2022-10-10 18:55:36 +0300

    Add base dependencies

---
+++ src/main/java/com/iam/shoutz/ShoutzApplication.java
@@ -0,0 +1,13 @@
+package com.iam.shoutz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoutzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoutzApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/iam/shoutz/ShoutzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iam.shoutz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoutzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
