c2f028cf5b23bf374472d7ab60a688600c285b92
Yaw Afrifa <isaac.afrifa3@yahoo.com>
2022-10-13 18:43:08 +0300

    Add customized TypeMismatch & InternalServerError exception handlers

--- src/main/java/com/iam/shoutz/exception/APIError.java
+++
@@ -1,9 +0,0 @@
-package com.iam.shoutz.exception;
-
-import com.fasterxml.jackson.annotation.JsonFormat;
-
-import java.time.LocalDateTime;
-
-public record APIError(int status, String message, String path,
-                       @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime timeStamp) {
-}
--- src/main/java/com/iam/shoutz/exception/ExceptionController.java
+++ src/main/java/com/iam/shoutz/exception/ExceptionController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
@@ -12,11 +13,27 @@ import java.time.LocalDateTime;
 @ControllerAdvice
 public class ExceptionController extends ResponseEntityExceptionHandler {
 
+    /* customize generic 500 error handler */
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDetails> handleAllGenericExceptions(Exception ex, WebRequest request) {
+        ErrorDetails errorDetails = new ErrorDetails(HttpStatus.INTERNAL_SERVER_ERROR.value(), ex.getMessage(),
+                request.getDescription(false), LocalDateTime.now());
+        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
     @ExceptionHandler(ResourceNotFound.class)
-    public ResponseEntity<APIError> handleClientNotFoundException(ResourceNotFound ex, WebRequest request) {
-        APIError errorDetails = new APIError(HttpStatus.NOT_FOUND.value(), ex.getMessage(),
-                request.getDescription(false) + "", LocalDateTime.now());
+    public ResponseEntity<ErrorDetails> handleClientNotFoundException(ResourceNotFound ex, WebRequest request) {
+        ErrorDetails errorDetails = new ErrorDetails(HttpStatus.NOT_FOUND.value(), ex.getMessage(),
+                request.getDescription(false), LocalDateTime.now());
         return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
     }
 
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<Object> handleMethodArgumentTypeMismatch(MethodArgumentTypeMismatchException ex,
+                                                                   WebRequest request) {
+        String error = "%s is not of the preferred type".formatted(ex.getValue());
+        ErrorDetails errorDetails = new ErrorDetails(HttpStatus.BAD_REQUEST.value(), error,
+                request.getDescription(false) , LocalDateTime.now());
+        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+    }
 }
