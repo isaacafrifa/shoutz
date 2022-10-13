3e163b7cb255be000faa585000283bacae14ff56
Yaw Afrifa <isaac.afrifa3@yahoo.com>
2022-10-13 13:53:10 +0300

    Add base post
    add created resource location to response

--- src/main/java/com/iam/shoutz/controller/UserController.java
+++ src/main/java/com/iam/shoutz/controller/UserController.java
@@ -3,11 +3,12 @@ package com.iam.shoutz.controller;
 import com.iam.shoutz.entity.User;
 import com.iam.shoutz.service.UserService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -25,5 +26,17 @@ public record UserController(UserService userService) {
     public User getOneUser(@PathVariable("id") Long id){
         log.info("Get Mapping for single user invoked");
         return userService.getUserById(id);
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<User> postUser(@RequestBody User dirtyUser){
+        User savedUser= userService.createUser(dirtyUser);
+        log.info("Post Mapping for user invoked using {}", savedUser.getUsername());
+        //add resource location URI as a response header
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest() //get currentURI i.e."/users"
+                .path("/{id}") //append "/id"
+                .buildAndExpand(savedUser.getId()) // replace {id} with user's id
+                .toUri();
+        return ResponseEntity.created(location).build();
     }
 }
