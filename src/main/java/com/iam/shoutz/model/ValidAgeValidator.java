79f184f09fc2c61f855444c94408865e7777ebc5
Afrifa <isaac.afrifa3@yahoo.com>
2022-10-19 17:48:55 +0300

    Write test for when user input is not of type localDate

--- src/main/java/com/iam/shoutz/model/ValidAgeValidator.java
+++ src/main/java/com/iam/shoutz/model/ValidAgeValidator.java
@@ -7,8 +7,8 @@ import java.time.LocalDate;
 import java.time.Period;
 
 /*
-This is customized constraint class to validate that a provided
-date of birth compared to the present date is above than 18yrs"
+This is customized constraint class to enforce the @ValidAge  (i.e. a provided
+date of birth compared to the present date should be equal or above 18yrs)
  */
 public class ValidAgeValidator implements ConstraintValidator<ValidAge, Object> {
     private static final int VALID_AGE=18;
