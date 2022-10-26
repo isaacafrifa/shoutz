0dd092434424f3134f7e161c4f8fe6c95f356f4f
Afrifa <isaac.afrifa3@yahoo.com>
2022-10-26 22:57:33 +0300

    Implement initial db insertion of posts

--- src/main/java/com/iam/shoutz/service/DBLoader.java
+++ src/main/java/com/iam/shoutz/service/DBLoader.java
@@ -1,6 +1,8 @@
 package com.iam.shoutz.service;
 
+import com.iam.shoutz.entity.Post;
 import com.iam.shoutz.entity.User;
+import com.iam.shoutz.repository.PostRepository;
 import com.iam.shoutz.repository.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -10,19 +12,28 @@ import java.time.Month;
 import java.util.Arrays;
 
 @Component
-public record DBLoader(UserRepository userRepository) implements CommandLineRunner {
+public record DBLoader(UserRepository userRepository, PostRepository postRepository) implements CommandLineRunner {
+
     @Override
     public void run(String... args) throws Exception {
-        initUsers();
+        init();
     }
 
-    /* Persisting initial users */
-    public void initUsers(){
-        User user1 = new User(null,"yawmens","Yaw", "Afrifa",
-                LocalDate.of(1992, Month.OCTOBER,5),null);
-        User user2 = new User(null,"miss_selim","Selim", "VanLare",
-                LocalDate.of(1992, Month.MARCH,23),null);
-        var users= Arrays.asList(user1,user2);
+    public void init() {
+        User user1 = new User(null, "yawmens", "Yaw", "Afrifa",
+                LocalDate.of(1992, Month.OCTOBER, 5), null, null);
+        User user2 = new User(null, "miss_selim", "Selim", "VanLare",
+                LocalDate.of(1992, Month.MARCH, 23), null, null);
+
+        Post post1 = new Post(null, "This is my first post",null, null, user1);
+        Post post2 = new Post(null, "This is my second post",null, null, user1);
+        Post post3 = new Post(null, "I'm refreshing my Microservices knowledge",null, null, user2);
+
+        var users = Arrays.asList(user1, user2);
+        var posts = Arrays.asList(post1, post2, post3);
         userRepository.saveAll(users);
+        postRepository.saveAll(posts);
     }
+
+
 }
