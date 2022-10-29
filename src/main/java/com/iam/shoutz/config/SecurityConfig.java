3fe3791368891076c310679357afc370d0d42954
Afrifa <isaac.afrifa3@yahoo.com>
2022-10-30 01:48:40 +0300

    Create SecurityConfig and implement basic auth

---
+++ src/main/java/com/iam/shoutz/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.iam.shoutz.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        return http
+                .csrf().disable() //disable csrf, we're dealing only with REST
+                .authorizeRequests(
+                        auth -> auth.anyRequest().authenticated()) // all requests should be authenticated
+                .httpBasic(Customizer.withDefaults())  // ask for basic auth credentials when request isn't authenticated
+                .build();
+    }
+}
